f6547704e902d0fb2feaecf9b3a334c179e72334
Miguel Flores <miguelflores@Miguels-MacBook-Air.local>
2023-09-23 13:06:09 -0700

    Desarrollo de metodo calcularDescuentoRenta

--- src/app/salario.service.ts
+++ src/app/salario.service.ts
@@ -8,8 +8,17 @@ export class SalarioService {
   constructor() { }
 
   calcularDescuentoRenta(salario: number): number {
-    // TODO: Calcular renta en base a salario y tabla de hacienda
-    return 0;
+    if (salario <= 487.60) {
+      return 0;
+    } else if (salario <= 642.85) {
+      return salario * 0.1;
+    } else if (salario <= 915.81) {
+      return salario * 0.1;
+    } else if (salario <= 2058.67) {
+      return salario * 0.2;
+    } else {
+      return salario * 0.3;
+    }
   }
 
   calcularDescuentoAFP(salario: number): number {
