9376cdf6233ab4a758f5f6d12f8a9f4f41a44954
Andrés <ia.andresalexander@ufg.edu.sv>
2023-09-23 12:37:38 -0600

    agregado funciones de descuento y calculo

--- src/app/salario.service.ts
+++ src/app/salario.service.ts
@@ -6,4 +6,24 @@ import { Injectable } from '@angular/core';
 export class SalarioService {
 
   constructor() { }
+
+  calcularDescuentoRenta(salario: number): number {
+    // TODO: Calcular renta en base a salario y tabla de hacienda
+    return 0;
+  }
+
+  calcularDescuentoAFP(salario: number): number {
+    // TODO: Calcular descuento de AFP
+    return 0;
+  }
+
+  calcularDescuentoISSS(salario: number): number {
+    // TODO: Calcular descuento de ISSS
+    return 0;
+  }
+
+  calcularSalarioLiquido(salario: number): number {
+    // TODO: Calcular descuentos
+    return 0;
+  }
 }
