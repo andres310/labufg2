c30ace36b2415c5d8405a337bcfae8878dada7bb
Andrés <ia.andresalexander@ufg.edu.sv>
2023-09-23 19:36:04 -0600

    agregado controlador de componente salario

--- src/app/salario/salario.component.ts
+++ src/app/salario/salario.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SalarioService } from '../salario.service';
 
 @Component({
   selector: 'app-salario',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./salario.component.css']
 })
 export class SalarioComponent {
+  salarioInput: number = 0;
 
+  // Para mostrar detalle de calculo
+  descuentoRenta: number = 0;
+  descuentoAFP: number = 0;
+  descuentoISSS: number = 0;
+
+  salarioLiquido: number = 0; // Resultado del calculo
+
+  constructor(private salarioService: SalarioService) {}
+
+  calcularSalario() {
+    this.salarioLiquido = this.salarioService.calcularSalarioLiquido(this.salarioInput);
+  }
+
+  calcularSalarioDetalle() {
+    this.descuentoRenta = this.salarioService.calcularDescuentoRenta(this.salarioInput);
+    this.descuentoAFP = this.salarioService.calcularDescuentoAFP(this.salarioInput);
+    this.descuentoISSS = this.salarioService.calcularDescuentoISSS(this.salarioInput);
+  }
 }
